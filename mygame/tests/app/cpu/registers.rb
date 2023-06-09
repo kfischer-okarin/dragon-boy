cdd4ca8752a204960997f7b6e3f136171fb13df2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-06-09 15:12:18 +0200

    Extract binary_equal method

--- mygame/tests/app/cpu/registers.rb
+++ mygame/tests/app/cpu/registers.rb
@@ -3,7 +3,7 @@ def test_cpu_registers_single_registers(_args, assert)
     registers = CPU::Registers.new
     registers.send :"#{register}=", 0b11001100
 
-    assert.equal! registers.send(register), 0b11001100
+    assert.binary_equal! registers.send(register), 0b11001100
   end
 end
 
@@ -11,7 +11,7 @@ def test_cpu_registers_single_registers_are_zero_by_default(_args, assert)
   %i[a b c d e f h l].each do |register|
     registers = CPU::Registers.new
 
-    assert.equal! registers.send(register), 0
+    assert.binary_equal! registers.send(register), 0
   end
 end
 
@@ -26,7 +26,7 @@ def test_cpu_registers_get_double_registers(_args, assert)
     registers.send :"#{register1}=", 0b11001100
     registers.send :"#{register2}=", 0b11110000
 
-    assert.equal! registers.send(double_register), 0b1100110011110000
+    assert.binary_equal! registers.send(double_register), 0b1100110011110000
   end
 end
 
@@ -40,7 +40,15 @@ def test_cpu_registers_set_double_registers(_args, assert)
     registers = CPU::Registers.new
     registers.send :"#{double_register}=", 0b1100110011110000
 
-    assert.equal! registers.send(register1), 0b11001100
-    assert.equal! registers.send(register2), 0b11110000
+    assert.binary_equal! registers.send(register1), 0b11001100
+    assert.binary_equal! registers.send(register2), 0b11110000
+  end
+end
+
+module GTK
+  class Assert
+    def binary_equal!(value, expected)
+      equal! value, expected, "Expected 0b#{value.to_s(2)}\n   to be 0b#{expected.to_s(2)}"
+    end
   end
 end
