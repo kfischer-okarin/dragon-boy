56318aa1bf442da708ee82c715f9bbe27fb82496
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-06-21 21:26:32 +0200

    Refactor tests

--- mygame/tests/app/program.rb
+++ mygame/tests/app/program.rb
@@ -1,31 +1,30 @@
-def test_program_parse_operation_without_arguments(_args, assert)
-  program = Program.new "\x00"
+[
+  {
+    name: :operation_without_arguments,
+    program_code: "\x00",
+    expected: { type: :NOP, arguments: [] }
+  },
+  {
+    name: :operation_with_immediate_register_arguments,
+    program_code: "\x09",
+    expected: { type: :ADD, arguments: [:HL, :BC] }
+  },
+  {
+    name: :operation_with_immediate_n8_arguments,
+    program_code: "\x06\x42",
+    expected: { type: :LD, arguments: [:B, 0x42] }
+  },
+  {
+    name: :operation_with_immediate_n16_arguments,
+    program_code: "\x21\x42\x33",
+    expected: { type: :LD, arguments: [:HL, 0x3342] }
+  }
+].each do |test_case|
+  define_method "test_program_parse_#{test_case[:name]}" do |_args, assert|
+    program = Program.new test_case[:program_code]
 
-  operation = program.parse_operation(0)
+    operation = program.parse_operation(0)
 
-  assert.equal! operation, { type: :NOP, arguments: [] }
-end
-
-def test_program_parse_operation_with_immediate_register_arguments(_args, assert)
-  program = Program.new "\x09"
-
-  operation = program.parse_operation(0)
-
-  assert.equal! operation, { type: :ADD, arguments: [:HL, :BC] }
-end
-
-def test_program_parse_operation_with_immediate_n8_arguments(_args, assert)
-  program = Program.new "\x0E\x42"
-
-  operation = program.parse_operation(0)
-
-  assert.equal! operation, { type: :LD, arguments: [:C, 0x42] }
-end
-
-def test_program_parse_operation_with_immediate_n16_arguments(_args, assert)
-  program = Program.new "\x21\x42\x33"
-
-  operation = program.parse_operation(0)
-
-  assert.equal! operation, { type: :LD, arguments: [:HL, 0x3342] }
+    assert.equal! operation, test_case[:expected]
+  end
 end
