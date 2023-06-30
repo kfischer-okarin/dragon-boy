30ed4058fe1ea99c5085c8c9fbf80137ca968346
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-07-01 01:27:49 +0900

    Set Debugger MemoryView offset initially to PC value

--- mygame/app/screens/debugger.rb
+++ mygame/app/screens/debugger.rb
@@ -7,6 +7,7 @@ module Screens
       registers_view_h = 250
       @registers_view = UI::RegistersView.new(game_boy.registers, x: 1080, y: 0, w: 200, h: registers_view_h)
       @memory_view = UI::MemoryView.new(game_boy.memory, x: 640, y: registers_view_h, w: 640, h: 720 - registers_view_h)
+      @memory_view.offset = game_boy.registers.pc & 0xFFF0
     end
 
     def tick(args)
