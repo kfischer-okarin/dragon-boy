bac09f856a7d7806f29df8f7c3949213e6c1d48e
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-07-01 02:03:33 +0900

    Load rom content at beginning of memory without boot rom

--- mygame/app/memory.rb
+++ mygame/app/memory.rb
@@ -1,6 +1,6 @@
 class Memory
-  def initialize(rom:, boot_rom:)
-    @content = boot_rom.bytes
+  def initialize(rom:, boot_rom: nil)
+    @content = boot_rom ? boot_rom.bytes : rom.bytes
   end
 
   def [](address)
--- mygame/tests/app/memory.rb
+++ mygame/tests/app/memory.rb
@@ -3,3 +3,9 @@ def test_memory_loads_boot_rom_at_0x0000_if_specified(_args, assert)
 
   assert.equal! memory[0x0000..0x0002], [0x01, 0x02, 0x03]
 end
+
+def test_memory_loads_rom_at_0x0000_if_no_boot_rom_is_specified(_args, assert)
+  memory = Memory.new rom: "\x03\x02\x01"
+
+  assert.equal! memory[0x0000..0x0002], [0x03, 0x02, 0x01]
+end
