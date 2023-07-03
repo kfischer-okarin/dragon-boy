ed0cbabdde25658a19af612db0883042aa48b220
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-07-04 00:22:13 +0900

    Display Sound Status in MiscView

--- mygame/app/ui/misc_info_view.rb
+++ mygame/app/ui/misc_info_view.rb
@@ -14,9 +14,20 @@ module UI
 
     def render(gtk_outputs)
       gtk_outputs.primitives << [
-        { x: @x, y: @y, w: @w + 1, h: @h + 1, r: 0, g: 0, b: 0 }.border!,
-        { x: @x + 10, y: top - 10, text: "Cycles: #{@game_boy.cpu.cycles}" }.label!
+        { x: @x, y: @y, w: @w + 1, h: @h + 1, r: 0, g: 0, b: 0 }.border!
       ]
+
+      x = @x + 10
+      y = top - 10
+      gtk_outputs.primitives << { x: x, y: y, text: "Cycles: #{@game_boy.cpu.cycles}" }.label!
+
+      y -= 20
+      begin
+        sound_status = @game_boy.io.sound_on? ? 'ON' : 'OFF'
+      rescue
+        sound_status = '---'
+      end
+      gtk_outputs.primitives << { x: x, y: y, text: "Sound: #{sound_status}" }.label!
     end
   end
 end
