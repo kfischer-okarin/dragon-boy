7d6d3df87e306b7fd8c435c8e6cea906a87dd812
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-07-14 09:25:42 +0900

    tests: Move exception_raised! to separate file + add comments

---
+++ mygame/tests/assertions.rb
@@ -0,0 +1,48 @@
+module GTK
+  class Assert
+    # Asserts that the given block raises an exception.
+    # You can optionally specify the exception class that is expected to be raised.
+    # If you don't specify an exception class, any exception will be accepted.
+    #
+    #   assert.exception_raised! do
+    #     raise 'Boom!!!'
+    #   end
+    #
+    #   assert.exception_raised!(RuntimeError) do
+    #     raise 'Boom!!!'
+    #   end
+    #
+    #   assert.exception_raised!(RuntimeError, 'No boom?') do
+    #     # No boom
+    #   end
+    def exception_raised!(exception_class_or_message = nil, message = nil)
+      if exception_class_or_message.is_a?(String) || exception_class_or_message.nil?
+        exception_raised = false
+        begin
+          yield
+        rescue StandardError
+          exception_raised = true
+        end
+
+        if !exception_raised
+          raise "Expected exception to be raised but no exception was raised.\n#{exception_class_or_message}"
+        end
+      else
+        raised_exception_class = nil
+        begin
+          yield
+        rescue StandardError => e
+          raised_exception_class = e.class
+        end
+
+        if raised_exception_class.nil?
+          raise "Expected exception #{exception_class} to be raised but no exception was raised.\n#{message}"
+        elsif !raised_exception_class.ancestors.include?(exception_class)
+          raise "Expected exception #{exception_class} to be raised but #{raised_exception_class} was raised.\n#{message}"
+        end
+      end
+
+      @assertion_performed = true
+    end
+  end
+end
--- mygame/tests/test_helper.rb
+++ mygame/tests/test_helper.rb
@@ -1,36 +1,4 @@
-module GTK
-  class Assert
-    def exception_raised!(exception_class_or_message = nil, message = nil)
-      if exception_class_or_message.is_a?(String) || exception_class_or_message.nil?
-        exception_raised = false
-        begin
-          yield
-        rescue StandardError
-          exception_raised = true
-        end
-
-        if !exception_raised
-          raise "Expected exception to be raised but no exception was raised.\n#{exception_class_or_message}"
-        end
-      else
-        raised_exception_class = nil
-        begin
-          yield
-        rescue StandardError => e
-          raised_exception_class = e.class
-        end
-
-        if raised_exception_class.nil?
-          raise "Expected exception #{exception_class} to be raised but no exception was raised.\n#{message}"
-        elsif !raised_exception_class.ancestors.include?(exception_class)
-          raise "Expected exception #{exception_class} to be raised but #{raised_exception_class} was raised.\n#{message}"
-        end
-      end
-
-      @assertion_performed = true
-    end
-  end
-end
+require 'tests/assertions.rb'
 
 def build_cpu
   CPU.new registers: Registers.new, memory: Memory.new
