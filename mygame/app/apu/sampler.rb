6c12efddae1c50f72de3d59282e261a5878241c3
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-07-25 10:14:23 +0900

    Implement Sampler#samples_until_cycle
    Which will be necessary for the other modules like volume/frequency sweep

--- mygame/app/apu/sampler.rb
+++ mygame/app/apu/sampler.rb
@@ -9,6 +9,9 @@ class APU
       @sample_index = 0
       @current_sample = @sample[@sample_index]
       @next_sample_cycle = @sample_period
+      @output_sample_cycles = (0..@output_sample_rate).map do |index|
+        (index * @clock_frequency).idiv(@output_sample_rate)
+      end
       self.output_sample_index = 0
     end
 
@@ -25,7 +28,32 @@ class APU
       result
     end
 
+    def samples_until_cycle(target_cycle)
+      result = []
+      return result if target_cycle == @cycle
+
+      if target_cycle < @cycle # We need to loop around first
+        until @output_sample_index.zero?
+          result << @current_sample
+          move_to_next_output_sample
+        end
+      end
+
+      while @cycle <= target_cycle
+        result << @current_sample
+        move_to_next_output_sample
+      end
+      @cycle = target_cycle
+
+      result
+    end
+
     private
+
+    def move_to_next_output_sample
+      self.output_sample_index = @output_sample_index + 1
+      next_sample if @cycle >= @next_sample_cycle
+    end
 
     def next_sample
       @sample_index = (@sample_index + 1) % @sample.length
@@ -38,7 +66,7 @@ class APU
     end
 
     def output_sample_index=(value)
-      @cycle = (value * @clock_frequency).idiv(@output_sample_rate)
+      @cycle = @output_sample_cycles[value]
       @output_sample_index = value
     end
   end
--- mygame/tests/app/apu/sampler.rb
+++ mygame/tests/app/apu/sampler.rb
@@ -56,3 +56,48 @@ def test_sampler_next_samples_indivisible_sample_rates(_args, assert)
   # Output 2   2     0     1     2     0     1
   assert.equal! output_samples, [0x2, 0x2, 0x0, 0x1, 0x2, 0x0, 0x1]
 end
+
+def test_sampler_samples_until_cycle(_args, assert)
+  sampler = APU::Sampler.new(
+    clock_frequency: 100,
+    output_sample_rate: 20,
+    sample_period: 10,
+    sample: [0x0, 0x1, 0xF]
+  )
+
+  output_samples = sampler.samples_until_cycle(23)
+
+  # Clock  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
+  # Sample 0                   1                   F
+  # Output 0         0         1         1         F         F
+  assert.equal! output_samples, [0x0, 0x0, 0x1, 0x1, 0xF]
+
+  output_samples = sampler.samples_until_cycle(25)
+
+  assert.equal! output_samples, [0xF]
+
+  assert.equal! sampler.samples_until_cycle(25), [] # Don't return the same sample twice
+end
+
+def test_sampler_samples_until_cycle_wrap_around(_args, assert)
+  sampler = APU::Sampler.new(
+    clock_frequency: 20,
+    output_sample_rate: 7,
+    sample_period: 3,
+    sample: [0x0, 0x1, 0x2]
+  )
+
+  output_samples = sampler.samples_until_cycle(16)
+
+  # Clock  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9
+  # Sample 0     1     2     0     1     2     0
+  # Output 0   0     1     2     0     1     2
+  assert.equal! output_samples, [0x0, 0x0, 0x1, 0x2, 0x0, 0x1]
+
+  output_samples = sampler.samples_until_cycle(5)
+
+  # Clock  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9
+  # Sample   1     2     0     1     2     0     1
+  # Output 0   1     2     0     1     2     0
+  assert.equal! output_samples, [0x2, 0x0, 0x1, 0x2]
+end
